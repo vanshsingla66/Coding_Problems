43e4386fdf47474cf349d6b3d98e8afa592ebfff
vansh <singlavansh125@gmail.com>
2026-09-21 22:36:30 +0530

    Three_Piles_Question

--- CODEFORCES/Three_Piles.java
+++ CODEFORCES/Three_Piles.java
@@ -1,7 +1,27 @@
 package CODEFORCES;
 import java.util.*;
 public class Three_Piles {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        for(int i =0; i<t; i++){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+ 
+            long diff = Math.abs(a - b);
+ 
+            long ans;
+ 
+            if(a>b){
+                ans = diff + c;
+            } else {
+                ans = Math.max(diff, Math.abs(diff - c));
+            }
+ 
+            System.out.println(ans);
+        }
+    }
     
 }
