97ce6c90acdf11e58e44fa3483fd69baaf12b9e0
Vansh <singlavansh125@gmail.com>
2026-09-24 11:56:50 +0530

    Time: 224 ms (8.56%), Space: 111.5 MB (37.27%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -7,15 +7,11 @@ class MedianFinder {
     }
     
     public void addNum(int num) {
-        if(max.isEmpty()){
-            max.add(num);
-        }
-        else{
-            max.add(num);
-            min.add(max.poll());
-            if(min.size()>max.size()){
-                max.add(min.poll());
-            }
+        
+        max.add(num);
+        min.add(max.poll());
+        if(min.size()>max.size()){
+            max.add(min.poll());
         }
     }
     
